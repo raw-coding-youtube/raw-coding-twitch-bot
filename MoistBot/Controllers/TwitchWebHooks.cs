d1277cb44f38d07ba50d33e3101cc62463393ecf
Anton Wieslander <atwieslander@gmai.com>
2020-06-20 22:05:30 +0100

    systemd and log

--- MoistBot/Controllers/TwitchWebHooks.cs
+++ MoistBot/Controllers/TwitchWebHooks.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using TwitchLib.Api;
 
 namespace MoistBot.Controllers
@@ -7,9 +12,25 @@ namespace MoistBot.Controllers
     [Route("api/twitch-web-hooks")]
     public class TwitchWebHooks : ControllerBase
     {
-        public IActionResult Index()
+        private readonly ILogger<TwitchWebHooks> _logger;
+
+        public TwitchWebHooks(ILogger<TwitchWebHooks> logger)
         {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index()
+        {
+            using var reader = new StreamReader(Request.Body, Encoding.UTF8);
+            var payload = await reader.ReadToEndAsync();
+            _logger.LogInformation(payload);
             return Ok();
         }
+    }
+
+    public class Payload
+    {
+        public Dictionary<string, string> Data { get; set; }
     }
 }
