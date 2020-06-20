d2928a0affba05f04ce91e303547f101184540d2
Anton Wieslander <atwieslander@gmai.com>
2020-06-20 20:58:53 +0100

    build & point

---
+++ MoistBot/Controllers/Home.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MoistBot.Controllers
+{
+    [ApiController]
+    [Route("api/home")]
+    public class Home : ControllerBase
+    {
+
+        [HttpGet]
+        public string Index()
+        {
+            return "Hello World!";
+        }
+    }
+}
